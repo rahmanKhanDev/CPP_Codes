293973525cb850e65c8cc02679c11f1bcd8c1c80
rahmanKhanDev <arkhan00555@gmail.com>
2024-10-17 20:29:09 +0530

    updated structure

--- datatypes/structure.cpp
+++ datatypes/structure.cpp
@@ -5,21 +5,25 @@ using namespace std;
 {
     int eId;
     string name;
-    float salary;
+    int salary;
     // string favColors[];
 } ;
 
 int main()
 {
     struct  employee rahman; // structure ref
+    struct employee sandhya;
     // assign values to employee rahman
     rahman.eId = 101;
     rahman.name = "rahman khan";
-    rahman.salary = 7865.09f;
+    rahman.salary = 9870;
     // rahman.favColors = {"white", "black", "red"};
-
+    sandhya.eId = 3106;
+    sandhya.name = "sandhya";
+    sandhya.salary = 5000;
     //access values of employee rahman
     cout<<"emp id:"<<rahman.eId<<"\n"<<"emp name:"<<rahman.name<<"\n"<<"emp salary:"<<rahman.salary<<"\n";
+      cout<<"emp id:"<<sandhya.eId<<"\n"<<"emp name:"<<sandhya.name<<"\n"<<"emp salary:"<<sandhya.salary<<"\n";
     // int size=sizeof(rahman.favColors)/sizeof(rahman.favColors[0]);
   
     
